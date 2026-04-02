62800e1e526704d3766fb08b7f7f1bcfc4500b37
Sergio Gabriel <sergio.gabriel@catskillet.com>
2026-04-02 02:02:03 -0300

    initial commit: Django with app products

---
+++ products/migrations/0001_initial.py
@@ -0,0 +1,65 @@
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Brand',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, verbose_name='Nome')),
+                ('is_active', models.BooleanField(default=True, verbose_name='Ativo?')),
+                ('description', models.TextField(blank=True, null=True, verbose_name='Descrição')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criada em')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Atualizado em')),
+            ],
+            options={
+                'verbose_name': 'Marca',
+                'verbose_name_plural': 'Marcas',
+                'ordering': ['name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, verbose_name='Nome')),
+                ('is_active', models.BooleanField(default=True, verbose_name='Ativo?')),
+                ('description', models.TextField(blank=True, null=True, verbose_name='Descrição')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criada em')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Atualizado em')),
+            ],
+            options={
+                'verbose_name': 'Categoria',
+                'verbose_name_plural': 'Categorias',
+                'ordering': ['name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, verbose_name='Nome')),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Preço')),
+                ('is_active', models.BooleanField(default=True, verbose_name='Ativo?')),
+                ('description', models.TextField(blank=True, null=True, verbose_name='Descrição')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criada em')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Atualizado em')),
+                ('brand', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='products', to='products.brand', verbose_name='Marca')),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='products', to='products.category', verbose_name='Categoria')),
+            ],
+            options={
+                'verbose_name': 'Produto',
+                'verbose_name_plural': 'Produtos',
+                'ordering': ['name'],
+            },
+        ),
+    ]
